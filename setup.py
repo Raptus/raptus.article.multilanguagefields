2cb11909f1a889fa4ecde608b32273a4e07a3a50
Jonas Wagner <jwagner@raptus.com>
2012-11-02 01:16:48 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b11'
 setup(name='raptus.article.multilanguagefields',
       version=version,
       description="Provides support for raptus.multilanguagefields in article",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "MANUAL.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -16,7 +16,7 @@ setup(name='raptus.article.multilanguagefields',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.article/raptus.article.multilanguagefields',
+      url='https://github.com/Raptus/raptus.article.multilanguagefields',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.article'],
